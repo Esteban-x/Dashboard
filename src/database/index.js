b236e8ce4eda1059af0dd62b97fe2bd2b997e0fa
unknown <esteprowork@gmail.com>
2024-01-17 01:06:57 +0100

    env var

--- src/database/index.js
+++ src/database/index.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const connectToDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://tiagzo:free@cluster0.b5pvpka.mongodb.net/dashboard?retryWrites=true&w=majority"
-    );
+    await mongoose.connect(process.env.DATABASE);
     console.log("Connecté à mongodb");
   } catch (error) {
     console.log(error);
